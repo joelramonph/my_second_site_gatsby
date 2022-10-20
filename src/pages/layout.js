aecf70b48f95ca6cdb24825ee11fe41e79987f83
Joel Parra <joelramonph@gmail.com>
2022-10-20 17:39:17 -0400

    add styles

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,18 +1,24 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle} from '../componets/layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+    <div className={container}>
+      <title className={heading}>{pageTitle}</title>
+      <nav className={navLinks}>
+        <ul className={navLinkItem}>
+          <li className={navLinkText}><Link to="/">Home</Link></li>
+          <li className={navLinkText}><Link to="/about">About</Link></li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={siteTitle}>{pageTitle}</h1>
         {children}
       </main>
     </div>
